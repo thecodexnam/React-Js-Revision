ff11b0e6a0fdb619feea8d382a2bce50d2a8d189
namanydv <namanofficial91@gmail.com>
2025-11-29 12:10:07 +0530

    feat: initialize React project with Vite setup
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Developed App.jsx component with a simple UI and a button.
    - Added React logo SVG in assets.
    - Set up global styles in index.css.
    - Implemented main entry point in main.jsx.
    - Configured Vite in vite.config.js for React support.

---
+++ react-revision/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <h1>Naman Yadav</h1>
+      <img
+        src="https://imgs.search.brave.com/QRO8_ocyX5U489An7UlMJZb2SouOEsq5M-sqbvM94WM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctam9z/VkhCRW9GRjFXeGZP/Zk5DQXJkLmpwZWc"
+        alt="Naman"
+      />
+      <ul>
+        <li>Invert new traffic Light</li>
+        <li>Reherce a Movie scene</li>
+        <li>code daily for improvement</li>
+      </ul>
+      <button onClick={al}>Click ME</button>
+    </div>
+  );
+}
+
+export default App;
