0821e8c2af6a550b9f0427dc5e1eb65b67d40315
namanydv <namanofficial91@gmail.com>
2025-11-29 12:49:32 +0530

    inserting img

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
   }
   const arr = ["naman","vishnu","rohan"]
 
+  let path = "https://imgs.search.brave.com/QRO8_ocyX5U489An7UlMJZb2SouOEsq5M-sqbvM94WM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctam9z/VkhCRW9GRjFXeGZP/Zk5DQXJkLmpwZWc"; 
+
   function operation(a,b,op){
     let result =0;
     if(op === "+"){
@@ -27,6 +29,7 @@ function App() {
       <h1>{age?age:"Age not found"}</h1>
       <h1>{obj.email}</h1>
       <h1>{arr[2]}</h1>
+      <img src={path}/>
       
 
       {/* <h1>Naman Yadav</h1>
