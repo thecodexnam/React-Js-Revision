1f5b008009c6bf14c2fe9ee75c565b6c23e68689
namanydv <namanofficial91@gmail.com>
2025-12-02 15:03:49 +0530

    toogle feature

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -1,60 +1,79 @@
-function Fruit(name){
-  alert("your Fruit Name is " + name)
-}
+// function Fruit(name){
+//   alert("your Fruit Name is " + name)
+// }
 
-function App() {
+// function App() {
 
-  let age = 20;
-  const obj ={
-    name:"Naman",
-    email:"naman@gmail.com",
-    passion:"Software Engineer"
-  }
-  const arr = ["naman","vishnu","rohan"]
+//   let age = 20;
+//   const obj ={
+//     name:"Naman",
+//     email:"naman@gmail.com",
+//     passion:"Software Engineer"
+//   }
+//   const arr = ["naman","vishnu","rohan"]
 
-  let path = "https://imgs.search.brave.com/QRO8_ocyX5U489An7UlMJZb2SouOEsq5M-sqbvM94WM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctam9z/VkhCRW9GRjFXeGZP/Zk5DQXJkLmpwZWc"; 
+//   let path = "https://imgs.search.brave.com/QRO8_ocyX5U489An7UlMJZb2SouOEsq5M-sqbvM94WM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctam9z/VkhCRW9GRjFXeGZP/Zk5DQXJkLmpwZWc"; 
 
-  function operation(a,b,op){
-    let result =0;
-    if(op === "+"){
-      return result = a+b;
-    }
-    if(op ==="-"){
-     return result =  a-b;
-    }
-    else{
-      return "Not Found"
-    }
-  }
+//   function operation(a,b,op){
+//     let result =0;
+//     if(op === "+"){
+//       return result = a+b;
+//     }
+//     if(op ==="-"){
+//      return result =  a-b;
+//     }
+//     else{
+//       return "Not Found"
+//     }
+//   }
   
-  // const Fruit = (name) =>{
-  //   alert(name);
-  // }
+//   // const Fruit = (name) =>{
+//   //   alert(name);
+//   // }
 
-  return (
-    <div>
-      <h1>{operation(100,33,"-")}</h1>
-      <h1>{age?age:"Age not found"}</h1>
-      <h1>{obj.email}</h1>
-      <h1>{arr[2]}</h1>
-      <img src={path}/>
-      <button onClick={()=>Fruit("Apple")}>Apple</button>
-      <button onClick={()=>Fruit("Banana")}>Banana</button>
+//   return (
+//     <div>
+//       <h1>{operation(100,33,"-")}</h1>
+//       <h1>{age?age:"Age not found"}</h1>
+//       <h1>{obj.email}</h1>
+//       <h1>{arr[2]}</h1>
+//       <img src={path}/>
+//       <button onClick={()=>Fruit("Apple")}>Apple</button>
+//       <button onClick={()=>Fruit("Banana")}>Banana</button>
       
 
-      {/* <h1>Naman Yadav</h1>
-      <img
-        src="https://imgs.search.brave.com/QRO8_ocyX5U489An7UlMJZb2SouOEsq5M-sqbvM94WM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctam9z/VkhCRW9GRjFXeGZP/Zk5DQXJkLmpwZWc"
-        alt="Naman"
-      />
-      <ul>
-        <li>Invert new traffic Light</li>
-        <li>Reherce a Movie scene</li>
-        <li>code daily for improvement</li>
-      </ul>
-      <button onClick={al}>Click ME</button> */}
+//       {/* <h1>Naman Yadav</h1>
+//       <img
+//         src="https://imgs.search.brave.com/QRO8_ocyX5U489An7UlMJZb2SouOEsq5M-sqbvM94WM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZ2V0aW1nLmFp/L21lZGlhL2dldGlt/Z19haV9pbWctam9z/VkhCRW9GRjFXeGZP/Zk5DQXJkLmpwZWc"
+//         alt="Naman"
+//       />
+//       <ul>
+//         <li>Invert new traffic Light</li>
+//         <li>Reherce a Movie scene</li>
+//         <li>code daily for improvement</li>
+//       </ul>
+//       <button onClick={al}>Click ME</button> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react'
+import User from './User';
+
+const App = () => {
+  const[display,setDisplay] = useState(false);
+  return (
+    <div>
+      
+      <h1>Toggle in React Js</h1>
+      <button onClick={()=>setDisplay(!display)}>Toggle</button>
+      {
+        display?<User/>:null
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
