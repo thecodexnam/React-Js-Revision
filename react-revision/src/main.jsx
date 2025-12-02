3528adaf0ddce5a072a9e846a3d71e4476571d44
namanydv <namanofficial91@gmail.com>
2025-12-02 14:05:46 +0530

    importing a state component in the main jsx file

--- react-revision/src/main.jsx
+++ react-revision/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import State from './State.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    {/* <App/> */}
+    <State/>
   </StrictMode>,
 )
  
