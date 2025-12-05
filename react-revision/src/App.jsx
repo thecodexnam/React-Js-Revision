b08b073a713daaa0e8dd0df83cf144ee7f2bdf36
namanydv <namanofficial91@gmail.com>
2025-12-05 14:16:27 +0530

    feat: add state management for student name and update User component title

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import User from './User'
 import College from './College'
 import Student from './Student'
@@ -29,14 +29,24 @@ const App = () => {
   }
 
 let collegeNames = ['IIT','DU','IITM','NIT','MIT']
+const[student,setStudent] = useState()
 
   return (
     <div>
       <h1>Props in React JS</h1>
 
-      <Student name="Sam"/>
+
+      { student && <Student name={student}/>  }
+      <button onClick={()=>setStudent("Naman Yadav")}>Update Student Name</button>
+
+
       {/* <User name={name} age={age}/> */}
-      <College names={collegeNames} />
+      {/* <College name={collegeNames[0]} />
+      <College name={collegeNames[1]} />
+      <College name={collegeNames[2]} />
+      <College name={collegeNames[3]} /> */}
+
+
       <User user={userObject} />
       <User user={userObject1} />
       <User user={userObject2} />
--- react-revision/src/User.jsx
+++ react-revision/src/User.jsx
@@ -3,7 +3,7 @@ const User = ({user}) =>{
   return(
     <div>
       <hr/>
-      <h1>User Componet</h1>
+      <h1>User Component</h1>
       <h1>Your name is {user.name} and your age is {user.age} Your gmail is {user.email} and address is {user.address}</h1>
     </div>
   )
