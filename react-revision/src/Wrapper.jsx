d51f7d9e0f0f54ec45f5955d9af95c86d9dd856f
namanydv <namanofficial91@gmail.com>
2025-12-08 23:39:47 +0530

    Remove unnecessary whitespace in Wrapper component

--- react-revision/src/Wrapper.jsx
+++ react-revision/src/Wrapper.jsx
@@ -2,6 +2,7 @@ const Wrapper = ({children,color="Red"}) =>{
     return(
         <div style={{color:color,border:"3px solid black",width:"300px",padding:"10px",margin:"10px"}}>
             {children}
+            
         </div>
     )
 }
