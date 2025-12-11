d402e8124b2be990f911bde642c044bfbdc4babe
namanydv <namanofficial91@gmail.com>
2025-12-11 19:34:21 +0530

    BindingFile

--- react-revision/src/main.jsx
+++ react-revision/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import State from './State.jsx'
 import Counter from './Counter.jsx'
+import CheckBoxes from './CheckBoxes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    {/* <App/> */}
     {/* <State/>
     <Counter/> */}
+    <CheckBoxes/>
   </StrictMode>,
 )
  
