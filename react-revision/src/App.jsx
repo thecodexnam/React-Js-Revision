d6dd35f0667793966514a080a120bd80a4c89bc9
namanydv <namanofficial91@gmail.com>
2025-12-13 11:07:16 +0530

    reuse component in loop

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -1,86 +1,137 @@
-import React, { useState } from 'react'
-import User from './User'
-import Wrapper from './Wrapper'
+import React from 'react'
+import User from './User';
 
 const App = () => {
-  const[name,setName] = useState('');
-  const[password,setPassword] = useState('');
-  const[email,setEmail] = useState('');
 
-  const hanadleclear = () =>{
-    setEmail('')
-    setPassword('') 
-    setName('')
-  } 
- 
+      const UserData = [
+       { Name:"Naman",
+        age:20,
+        Address:"31B/1 Najafgarh, New Delhi - 110043",
+        email:"namantest@gmail.com",
+        id:1
+       },
+       { Name:"Daman",
+        age:29,
+        Address:"House-21 Mistic Fall, England - 5452",
+        email:"damontest@gmail.com",
+        id:2
+       },
+       { Name:"Stefen",
+        age:23,
+        Address:"House-210 Mistic Fall, England - 5452",
+        email:"stefentest@gmail.com",
+        id:3
+       }    
+    ]
+
   return (
-      <div>
-        <h1>Controlled Component</h1>
-        <form>
-          <input type='text' value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'/><br/><br/>
-          <input type='password' value={password}  onChange={(e)=>setPassword(e.target.value)} placeholder='Enter password'/><br/><br/>
-          <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email'/><br/><br/>
-          <button>Submit</button>
-          <button type='button' onClick={hanadleclear}>clear</button>
-
-          <h1>{name}</h1>
-          <h1>{password}</h1>
-          <h1>{email}</h1>
-        </form>
-
-
-
-
-        {/* <h1>Task in React JS</h1>
-        <div style={{margin:"10px",padding:"10px"}}>
-        <input type='text' value={name}  onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'/> <br/>
-        <input type='number' value={age} onChange={(e)=>setage(e.target.value)} placeholder='Enter Age'/> <br/>
-        <input type='address' value={address} onChange={(e)=>setaddress(e.target.value)} placeholder='Enter Address'/> <br/>
-        <button disabled={!name || !age || !address} onClick={handleOutput}>Submit</button>
-
-        </div>
-
-        {showOutput && (
-          <>
-          <h2>Your Name is: {name}</h2>
-          <h2>Your Age is: {age}</h2>
-          <h2>Your Address is: {address}</h2>
-        </>
-        )} */}
-
-
-
-      {/* <h1>Get Input Filed Value</h1>
-      <input type='text' value={val} onChange={(event)=>setValue(event.target.value)} placeholder='Enter User Name'></input>
-      <button style={{margin:"10px"}} onClick={()=>setValue("")}>Clear</button>
-      <h1>{val}</h1> */}
-
-
-
-
-      {/* <h1>Props in React JS</h1>
-      <Wrapper color="Orange">
-        <h1>Hello I'm Wrapper</h1>
-      </Wrapper>
-
-      <Wrapper>
-        <h1>Hello, I'm Naman Yadav</h1>
-        <h1 style={{color:"blue"}}>Hello i'm CodeXnam</h1>
-      </Wrapper> */}
-
-
-      {/* <User name='Naman Yadav'/>
-      <User/>
-      <User name='Rohan Mathur'/>
-      <User name='Vishnu Dubey'/>
-      <User/> */}
-      
-      {/* <User name="Naman Yadav"/>
-      <User name = "Rohan Mathur"/>
-      <User/>
-      <User name='Vishnu Dubey'/> */}
+    <div>
+      <h1>ReUse Component in Loop</h1>
+      {
+        UserData.map((user)=>(
+          <div key={user.id}>
+            <User user={user}/>
+          </div>
+        ))
+      }
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+// import User from './User'
+// import Wrapper from './Wrapper'
+
+// const App = () => {
+//   const[name,setName] = useState('');
+//   const[password,setPassword] = useState('');
+//   const[email,setEmail] = useState('');
+
+//   const hanadleclear = () =>{
+//     setEmail('')
+//     setPassword('') 
+//     setName('')
+//   } 
+ 
+//   return (
+//       <div>
+//         <h1>Controlled Component</h1>
+//         <form>
+//           <input type='text' value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'/><br/><br/>
+//           <input type='password' value={password}  onChange={(e)=>setPassword(e.target.value)} placeholder='Enter password'/><br/><br/>
+//           <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email'/><br/><br/>
+//           <button>Submit</button>
+//           <button type='button' onClick={hanadleclear}>clear</button>
+
+//           <h1>{name}</h1>
+//           <h1>{password}</h1>
+//           <h1>{email}</h1>
+//         </form>
+
+
+
+
+//         {/* <h1>Task in React JS</h1>
+//         <div style={{margin:"10px",padding:"10px"}}>
+//         <input type='text' value={name}  onChange={(e)=>setName(e.target.value)} placeholder='Enter Name'/> <br/>
+//         <input type='number' value={age} onChange={(e)=>setage(e.target.value)} placeholder='Enter Age'/> <br/>
+//         <input type='address' value={address} onChange={(e)=>setaddress(e.target.value)} placeholder='Enter Address'/> <br/>
+//         <button disabled={!name || !age || !address} onClick={handleOutput}>Submit</button>
+
+//         </div>
+
+//         {showOutput && (
+//           <>
+//           <h2>Your Name is: {name}</h2>
+//           <h2>Your Age is: {age}</h2>
+//           <h2>Your Address is: {address}</h2>
+//         </>
+//         )} */}
+
+
+
+//       {/* <h1>Get Input Filed Value</h1>
+//       <input type='text' value={val} onChange={(event)=>setValue(event.target.value)} placeholder='Enter User Name'></input>
+//       <button style={{margin:"10px"}} onClick={()=>setValue("")}>Clear</button>
+//       <h1>{val}</h1> */}
+
+
+
+
+//       {/* <h1>Props in React JS</h1>
+//       <Wrapper color="Orange">
+//         <h1>Hello I'm Wrapper</h1>
+//       </Wrapper>
+
+//       <Wrapper>
+//         <h1>Hello, I'm Naman Yadav</h1>
+//         <h1 style={{color:"blue"}}>Hello i'm CodeXnam</h1>
+//       </Wrapper> */}
+
+
+//       {/* <User name='Naman Yadav'/>
+//       <User/>
+//       <User name='Rohan Mathur'/>
+//       <User name='Vishnu Dubey'/>
+//       <User/> */}
+      
+//       {/* <User name="Naman Yadav"/>
+//       <User name = "Rohan Mathur"/>
+//       <User/>
+//       <User name='Vishnu Dubey'/> */}
+//     </div>
+//   )
+// }
+
+// export default App
--- react-revision/src/User.jsx
+++ react-revision/src/User.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
-const User = ({name= "New User"}) => {
-  return (
-    <div>
-      <h1>Hi, {name}</h1>
+const User = ({user}) =>{
+  return(
+    <div style={{border:"2px solid",borderRadius:"10px",margin:"20px",padding:"10px"}}>
+      <h3>Name:<span style={{color:"red"}}>{user.Name}</span></h3>
+      <h3>Name:<span style={{color:"green"}}>{user.email}</span></h3>
+      <h3>Name:<span style={{color:"purple"}}>{user.age}</span></h3>
     </div>
   )
 }
 
-export default User
+export default User;
