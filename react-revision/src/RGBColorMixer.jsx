bd7f7e4e1055f5b5b656e784a897b171e7177c77
namanydv <namanofficial91@gmail.com>
2025-12-16 20:54:57 +0530

    Building a RGB color mixer in react js

--- react-revision/src/RGBColorMixer.jsx
+++ react-revision/src/RGBColorMixer.jsx
@@ -1,27 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const RGBColorMixer = () => {
-    const[red,setRed] = useState(10);
-    const[green,setGreen] = useState(5)
-    const[blue,setBlue] = useState(4)
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
 
-
+  //rgb('+red+','+green+','+blue+')
 
   return (
     <div>
-        <h1>RGB Color Mixer in React Js</h1>
-        <div style={{height:'200px',width:'200px',margin:'20px',background:`rgb${red,green,blue}`}}>
+      <h1>RGB Color Mixer in React Js</h1>
+      <div
+        style={{
+          height: "200px",
+          width: "200px",
+          margin: "20px",
+          background: "rgb(" + red + "," + green + "," + blue + ")",
+          borderRadius:'50%'
+        }}
+      ></div>
 
-        </div>
-
-        <label htmlFor='red'>Red</label>
-        <input type='range'onChange={(e)=>setRed(e.target.value)} min={0} max={255}/><br/>
-        <label htmlFor='green'>Green</label>
-        <input type='range' min={0} max={255}/><br/>
-        <label htmlFor='blue'>Blue</label>
-        <input type='range' min={0} max={255}/><br/>
+      <label htmlFor="red">Red</label>
+      <input
+        type="range"
+        onChange={(e) => setRed(e.target.value)}
+        min={0}
+        max={255}
+        value={red}
+      />
+      <br />
+      <label htmlFor="green">Green</label>
+      <input
+        type="range"
+        onChange={(e) => setGreen(e.target.value)}
+        min={0}
+        max={255}
+        value={green}
+      />
+      <br />
+      <label htmlFor="blue">Blue</label>
+      <input
+        type="range"
+        onChange={(e) => setBlue(e.target.value)}
+        min={0}
+        max={255}
+        value={blue}
+      />
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default RGBColorMixer
+export default RGBColorMixer;
