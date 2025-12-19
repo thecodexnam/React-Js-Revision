f387c80c19713d02793e6f3240589af71c7cbca6
namanydv <namanofficial91@gmail.com>
2025-12-19 11:16:00 +0530

    Learming Basic Bootstrap

--- react-revision/src/main.jsx
+++ react-revision/src/main.jsx
@@ -14,6 +14,7 @@ import ExternalCss from "./ExternalCss.jsx";
 import RGBColorMixer from "./RGBColorMixer.jsx";
 import InterviewPrep from "./InterviewPrep.jsx";
 import ModuleCss from "./ModuleCss.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
