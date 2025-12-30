e53dafe2ee519c702bb2b2cda3a2d6c12f9bbd86
namanydv <namanofficial91@gmail.com>
2025-12-30 11:00:13 +0530

    learning hooks in react js

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { Header } from './Component/Header.jsx'
 import { Hero } from './Component/Hero.jsx'
 import { Footer } from './Component/Footer.jsx'
+import LearningHooks from './LearningHooks.jsx'
 
 function App() {
   return (
-   <>
-   <Header/>
-   <Hero/>
-   <Footer/>
-   </>
+  //  <>
+  //  <Header/>
+  //  <Hero/>
+  //  <Footer/>
+  //  </>
+<>
+ <LearningHooks/>
+</>
+
   ) 
   
 }
---
+++ props/src/LearningHooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LearningHooks = () => {
+  return (
+    <div>
+        <h1>UseEfeect Hooks in React JS</h1>
+    </div>
+  )
+}
+
+export default LearningHooks;
