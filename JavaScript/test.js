4088b04d29cc811362f6f5e1f4c673450b4a71ae
namanydv <namanofficial91@gmail.com>
2026-01-02 16:37:24 +0530

    basic code

---
+++ JavaScript/test.js
@@ -0,0 +1,12 @@
+let obj={ ayush:95 , Hemant:92 , Dev:33 }
+
+for (val in obj) 
+{
+    console.log("The Name of Student is ",val ," and Marks is ",obj[val] )
+}
+
+let tableVal = 12;
+
+for(let i =1; i<=10; i++){
+    console.log(tableVal*i);
+}
