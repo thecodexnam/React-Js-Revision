aa33542f2af89275b43f96552a60adc4a13ae4c9
namanydv <namanofficial91@gmail.com>
2026-01-05 14:16:42 +0530

    levle6 question

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,2 +1,24 @@
-let a = document.getElementById('home')
-console.log(a.matches(".item"))
+// let result = true;
+
+// while (result) {
+//   let age = Number(prompt("Enter age"));
+
+//   if (age >= 18) {
+//     alert("You can drive");
+//   } else {
+//     alert("You cannot drive");
+//   }
+
+//   result = confirm("Do you want to run the program again?");
+// }
+
+
+// let a = document.querySelector('body')
+// let b = prompt("Enter background color: ")
+// a.style.backgroundColor = b;
+
+let x =document.getElementsByTagName('nav');
+x[0].firstElementChild.style.background = "brown"
+
+
+// x.fir style.color = "purple"l
