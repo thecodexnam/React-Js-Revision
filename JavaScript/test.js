c6c19ee74fce058c85e76b3f0e85ba0a36d0bd3b
namanydv <namanofficial91@gmail.com>
2026-01-05 10:46:57 +0530

    BOM (Browser object model)

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,3 +1,124 @@
-let a = "were you able to do it"
+// let arr = [10,20,30,40,50];
 
-console.log(a.replace("able","notAble"))
+// for(let i=0; i<=arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// let fi = (value,index)=>{
+//     console.log("Index Value of ",value," is ",index)
+// }
+
+// arr.forEach(fi)
+
+// let arr1 = [1,2,3]
+
+// let new_arr = arr1.map((val)=>{
+//     return val*val;
+// })
+
+// console.log(new_arr)
+
+// let n_arr = arr1.reduce((a,b)=>{
+//     return a*b;
+// })
+
+// console.log(n_arr);
+// let res =1 ;
+
+// for(let i = 1; i<=3;i++){
+//     res = res*i;
+// }
+
+// console.log(res)
+
+// const grades = [78,65,98,65,87,95];
+
+// const maximum = grades.reduce(getMax);
+
+// const minimum = grades.reduce((current,next)=>{
+//     return Math.min(current,next)
+// })
+
+// console.log(maximum);
+// console.log(minimum)
+
+// function getMax(acc,elm){
+//     return Math.max(acc,elm);
+// }
+
+// let name = "Naman"
+// Array.from(name);
+// console.log( typeof name)
+
+// L5 Q-1
+// let arr1 = [];
+
+// let noi = 9;
+// let inp;
+
+// for(let i =1; i<=noi; i++){
+//     inp = prompt(" Enter the value")
+//     arr1.push(inp);
+// }
+
+//L5 Q-2
+
+// let sum =0;
+
+//  for(let i =1;i<=27;i++){
+//    sum = sum+i;
+// }
+
+// console.log(sum)
+
+//L5 Q-3
+// let arr = [21,32,434,25,33,53,74,7,4,76,34,24,2,53,353,5,3,636,34]
+
+// let new_arr = arr.filter((val,inx)=>{
+//     return val%2 ==0;
+// })
+
+// console.log(new_arr);
+
+//L5 Q-4
+
+// const arr = [12,32,43,54,43,23,43];
+
+// const new_arr = arr.map((val)=>{
+//     return val*val*val;
+// })
+
+// console.log(new_arr);
+
+
+// let x = [1,2,3,4,5,6,7];
+// let num;
+
+// do{
+//     num = parseInt(prompt(""));
+//     x.push(num);
+// }while(num != 27)
+
+//     console.log(x)
+
+//L5 Q-5
+
+const arr = {
+    naman:40,
+    rohan:34,
+    karan:54
+}
+
+console.table(arr)
+console.info();
+
+
+
+
+
+
+
+
+
+
+
