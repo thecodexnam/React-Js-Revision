34b8dd727a80cc7fef0a727f9ed5fdb290949314
namanydv <namanofficial91@gmail.com>
2026-01-07 10:51:26 +0530

    Some insertAdjacentHTML

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,9 +1,37 @@
-let box = document.createElement('div')
-box.className = "naman"
-box.innerHTML = "<h1>Naman Yadav</h1>"
-document.body.append(box)
+// let box = document.createElement('div')
+// box.className = "naman"
+// box.innerHTML = "<h1>Naman Yadav</h1>"
 
-let a = document.createElement('h1')
-a.innerHTML = "I'm Heading 1 Tag"
 
-box.replaceWith(a)
+
+
+
+
+
+// Some insertAdjacentHTML
+
+// box.insertAdjacentHTML("beforebegin","<h1> beforebegin </h1>")
+// undefined
+// box.insertAdjacentHTML("afterbegin","<h1> Afterbegin </h1>")
+// undefined
+// box.insertAdjacentHTML("beforeend","<h1> beforeend </h1>")
+// undefined
+// box.insertAdjacentHTML("afterend","<h1> after end </h1>")
+
+
+
+// document.body.append(box)
+
+// let a = document.createElement('h1')
+// a.innerHTML = "I'm Heading 1 Tag"
+
+// box.replaceWith(a)
+
+
+// some insertion method
+
+// box.append(a)
+// box.prepend(a)
+// box.before(a)
+// box.after(a)
+// box.replaceWith(a)
