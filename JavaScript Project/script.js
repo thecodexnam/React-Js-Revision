96997946030fffe1b214f461d7dfc5af38e65c8c
namanydv <namanofficial91@gmail.com>
2026-01-09 14:18:11 +0530

    callback hell

--- JavaScript Project/script.js
+++ JavaScript Project/script.js
@@ -15,14 +15,38 @@
 //     console.log(result);
 // }
 
-function calFact(a,callback){
-    callback(a)
+// function calFact(a,callback){
+//     callback(a)
+// }
+
+// calFact(5,(a)=>{
+//     let result = 1;
+//     for(let i =1;i<=a;i++){
+//         result *= i;
+//     }
+//     console.log(result);
+// })
+
+function nam(){
+    console.log("i run after 5 sec")
 }
 
-calFact(5,(a)=>{
-    let result = 1;
-    for(let i =1;i<=a;i++){
-        result *= i;
-    }
-    console.log(result);
-})
+setTimeout(()=>{
+    nam()
+},5000)
+
+// function database(id,nextdata){
+//     setTimeout(()=>{
+//     console.log("Data",id);
+//     if(nextdata){
+//         nextdata()
+//     }
+//     },2000)
+
+// }
+
+// database(1,()=>{
+//     database(2,()=>{
+//         database(3)
+//     })
+// })
