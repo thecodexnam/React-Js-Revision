3e660a70cf020bf2ee57ff2686efb3418c0df3e7
namanydv <namanofficial91@gmail.com>
2026-01-12 21:33:21 +0530

    find function in js

---
+++ JavaScript/Script.js
@@ -0,0 +1,12 @@
+let arr = [10,45,67,89,23,12];
+
+const result = arr.find((item)=>item>12)
+console.log(result)
+
+const result1 = arr.findIndex((item)=>item>12)
+console.log(result1)
+
+const result2 = arr.findLast((item)=>item>12)
+console.log(result2)
+
+
