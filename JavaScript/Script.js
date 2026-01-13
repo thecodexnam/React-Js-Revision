cc1b2af85898870c2a6a8cfc75200b28b72e6b43
namanydv <namanofficial91@gmail.com>
2026-01-13 12:27:21 +0530

    scope in js

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,24 +1,32 @@
-// Heap Vs Stack Allocation in JavaScript
-//Heap - Heap is used for dynamic allocation it is referenceType
-//Stact - Stack is used for static allocation, it is primitive type
+// // Heap Vs Stack Allocation in JavaScript
+// //Heap - Heap is used for dynamic allocation it is referenceType
+// //Stact - Stack is used for static allocation, it is primitive type
 
 
-//Heap Example
-let obj = {
-    Name:"Naman",
-    course:"BCA"
+// //Heap Example
+// let obj = {
+//     Name:"Naman",
+//     course:"BCA"
+// }
+// console.log(obj.Name);
+
+// let obj1 = obj;
+// obj1.Name = "Rohan"
+// console.log(obj.Name);
+// console.log(obj1.Name)
+
+// // Stack
+
+// let a = "Karan"
+// let b = a;
+// b = "Mohit";
+// console.log(a);
+// console.log(5%10);
+
+function print(){
+    var a = "Naman"
+    console.log("Hello ",a)
 }
-console.log(obj.Name);
 
-let obj1 = obj;
-obj1.Name = "Rohan"
-console.log(obj.Name);
-console.log(obj1.Name)
-
-// Stack
-
-let a = "Karan"
-let b = a;
-b = "Mohit";
+print()
 console.log(a);
-console.log(5%10);
