05c9f60cc46a6c8ac0dc6c4e8f1d972670e3a62c
namanydv <namanofficial91@gmail.com>
2026-01-15 18:54:11 +0530

    array in js

---
+++ JavaScript/ArrayLoop.js
@@ -0,0 +1,16 @@
+let character = ["Steve","Eleven","Robin","Mike","Dustin","Lukas"]
+
+
+// FOREACH = IT used to loop through each item in array
+// character.forEach((val)=>{
+//     console.log(val," Also work in Stranger Things")
+// })
+
+
+
+// MAP = Create a new array by applying function to every item
+let num = [19,392,539,44,33,43,29,29,2942,4]
+let ans = num.map(num => num * num)
+console.log(ans);
+
+
