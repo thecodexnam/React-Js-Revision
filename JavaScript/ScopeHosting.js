6606438d31ddc3a31cf916fb7df7341979676da6
namanydv <namanofficial91@gmail.com>
2026-01-15 08:04:13 +0530

    Scope and Hosting in JS

---
+++ JavaScript/ScopeHosting.js
@@ -0,0 +1,43 @@
+// Scope = determine the accessibility of variable, function and object
+
+
+// function greet(){
+//     let a = "Naman"
+//     console.log(`Hello ${a}`)
+// }
+
+// //Block Scope {}, its not applicable for var
+
+// {
+//     let a =10;
+//     const b = 20;
+//     var c =console.log(a+b);
+// }
+
+// Hosting  = Hosting is used to declared the function and value in the top of the codeBase
+
+
+// console.log(name);
+// var name = "Naman Yadav";
+// console.log(name);
+
+
+// when the code is executed V8 is declared the scope then 
+/* 
+var name;
+console.log(undefined)
+name="Naman"
+console.log("Naman Yadav")
+
+*/
+
+
+ function mynam(){
+    console.log("This variable is declared from the mynam function")
+}
+
+let a = mynam()
+console.log(a);
+
+
+
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log(name);
+// var name = "Naman Yadav";
+// console.log(name);
